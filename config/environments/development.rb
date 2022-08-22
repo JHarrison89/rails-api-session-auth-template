bb932e795308979c300028aec38f5cbc6ec1cf08
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2022-08-22 17:05:33 +0100

    Setting up .env, dotenv gem & gitignore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: 'myrailsmail@gmail.com',
-    password: 'elyhzsrrjpvnifvz',
+    password: ENV['GOOGLE_APP_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
